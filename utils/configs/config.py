e4881fd679178dd991018c69da11b3ca97a77a42
Anton Rubisov <anton.rubisov@bain.com>
2025-07-27 10:44:02 -0400

    move logger to separate file

--- utils/configs/config.py
+++ utils/configs/config.py
@@ -1,5 +1,3 @@
-import logging
-
 from pydantic import EmailStr
 from pydantic_settings import BaseSettings
 
@@ -22,24 +20,4 @@ class Settings(BaseSettings):
         env_file_encoding = "utf-8"
 
 
-# one global instance you import everywhere
 settings = Settings()
-
-
-class LggWrapper:
-    def __init__(self, logger):
-        self.em = logger.critical  # EMERGENCY (mapped to CRITICAL)
-        self.a = logger.critical  # ALERT     (same as critical)
-        self.c = logger.critical  # CRITICAL
-        self.er = logger.error  # ERROR
-        self.w = logger.warning  # WARNING
-        self.n = logger.info  # NOTICE (mapped to INFO)
-        self.i = logger.info  # INFO
-        self.d = logger.debug  # DEBUG
-
-
-def setup_logger(level=logging.INFO) -> LggWrapper:
-    logging.basicConfig(
-        level=level, format="[%(levelname)s] %(message)s", handlers=[logging.StreamHandler()]
-    )
-    return LggWrapper(logging.getLogger())
